e4e6462c341b94426ccfe13319ffe36a8713680e
SHIVA73 <shiva@coresonant.com>
2024-10-04 13:01:19 +0530

    Update alter_table.sql

--- alter_table.sql
+++ alter_table.sql
@@ -1,18 +1,52 @@
--- Check if the column 'Department' exists in the 'finaltable' table
-SELECT COUNT(*)
-INTO @columnExists
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'finaltable'
-AND COLUMN_NAME = 'Department'
-AND TABLE_SCHEMA = 'sampletest';
+-- List of tables, columns, and desired data types
+SET @columnsToCheck = 'finaltable.Department VARCHAR(255), finaltable.EmployeeName VARCHAR(100), othertable.Age INT';
 
--- If the column does not exist, dynamically alter the table
-SET @alterTableStatement = IF(@columnExists = 0, 
-    'ALTER TABLE finaltable ADD COLUMN Department VARCHAR(255);', 
-    'SELECT "Column already exists";'
-);
+-- Loop through each table-column pair
+SET @counter = 1;
+SET @totalColumns = 3; -- Number of column-table pairs to check
 
--- Prepare and execute the dynamic SQL statement
-PREPARE stmt FROM @alterTableStatement;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+WHILE @counter <= @totalColumns DO
+    -- Extract table, column, and data type from the list
+    SET @tableColumnDataType = (SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(@columnsToCheck, ',', @counter), ',', -1));
+    SET @tableName = (SELECT SUBSTRING_INDEX(@tableColumnDataType, '.', 1));
+    SET @columnName = (SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(@tableColumnDataType, '.', 2), ' ', 1));
+    SET @desiredDataType = (SELECT SUBSTRING_INDEX(@tableColumnDataType, ' ', -1));
+
+    -- Check if the column exists in the current table
+    SELECT COUNT(*)
+    INTO @columnExists
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = @tableName
+    AND COLUMN_NAME = @columnName
+    AND TABLE_SCHEMA = 'sampletest';
+
+    -- If column exists, check the data type
+    IF @columnExists = 1 THEN
+        SELECT COLUMN_TYPE
+        INTO @currentDataType
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_NAME = @tableName
+        AND COLUMN_NAME = @columnName
+        AND TABLE_SCHEMA = 'sampletest';
+
+        -- Check if the data type matches the desired one
+        IF @currentDataType != @desiredDataType THEN
+            -- Alter the column data type
+            SET @alterColumnStatement = CONCAT('ALTER TABLE ', @tableName, ' MODIFY ', @columnName, ' ', @desiredDataType, ';');
+            PREPARE stmt FROM @alterColumnStatement;
+            EXECUTE stmt;
+            DEALLOCATE PREPARE stmt;
+        ELSE
+            SELECT CONCAT('Column ', @columnName, ' already has the desired data type in table ', @tableName);
+        END IF;
+    ELSE
+        -- Add the column if it doesn't exist
+        SET @alterTableStatement = CONCAT('ALTER TABLE ', @tableName, ' ADD COLUMN ', @columnName, ' ', @desiredDataType, ';');
+        PREPARE stmt FROM @alterTableStatement;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+
+    -- Move to the next column-table pair
+    SET @counter = @counter + 1;
+END WHILE;
